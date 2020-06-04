3039252459475ac6f20d53064a8ef47b78940e59
Tanya Hodges <tanyac52003@gmail.com>
2020-06-04 17:17:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_reader :roster
+
+def initialize(name) 
+  @roster = {}
+end 
+
+def add_student(name,grade)
+  unless @roster[grade]
+  @roster[grade] = []
+end
+
+def grade(number)
+  roster[number]
+end 
+
+def sort 
+  new_hash = {}
+  roster.keys.sort.each do |number|
+    new_hash[number] = roster[number].sort 
+  end 
+  new_hash
+end
+end 
+end 
